ab099d30a1fc6de96a9599dab6be4a459567e663
Ivan Lopez <ivanlmar@ext.inditex.com>
2024-10-01 17:41:18 +0200

    fix: paths not working

---
+++ code/boot/src/main/java/com/smoke/meteoservice/MeteoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.smoke.meteoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeteoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeteoServiceApplication.class, args);
+    }
+
+}
